71b940fa2d619d17f0bda64620a4a5b897f5625d
Roshan Jeffrin R <roshanjeffrinr@gmail.com>
2026-04-26 16:36:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, requests, json, asyncio
+import os, requests, json
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
